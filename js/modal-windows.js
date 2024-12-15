b9fb4b1680e2f84f7389678fe042aca7310fbd64
Makhach <mmaaamm666@gmail.com>
2024-12-15 03:47:18 +0300

    upd mod win

--- js/modal-windows.js
+++ js/modal-windows.js
@@ -1,17 +1,16 @@
 const feedback = document.querySelector('.header__chat');
 const feedbackWindow = document.querySelector('.feedback-wrapper');
-const close = document.querySelector('.close');
-const background = document.querySelector('.sidebar-background');
-const body = document.querySelector('body');
+const exit = document.querySelector('.close');
 
 feedback.addEventListener('click', function () {
+  
   feedbackWindow.classList.add('open');
   background.style.display = 'block';
-  close.classList.add('exit');
+  exit.classList.add('exit');
   body.style.overflowY = 'hidden';
 });
 
-close.addEventListener('click', function () {
+exit.addEventListener('click', function () {
    feedbackWindow.classList.remove('open');
   background.style.display = 'none';
   body.style.overflowY = 'visible';
