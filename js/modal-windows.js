675d3fe14b6425b9fe02b3484f27cbed4bc2700d
Makhach <mmaaamm666@gmail.com>
2024-12-16 00:51:22 +0300

    upd modwins

--- js/modal-windows.js
+++ js/modal-windows.js
@@ -1,23 +1,40 @@
-const feedback = document.querySelector('.header__chat');
+const feedbacks = document.querySelectorAll('.chat');
 const feedbackWindow = document.querySelector('.feedback-wrapper');
 const exit = document.querySelector('.close');
+const callbacks = document.querySelectorAll('.call');
+const callbackWindow = document.querySelector('.callback-wrapper');
 
-feedback.addEventListener('click', function () {
-  
-  feedbackWindow.classList.add('open');
-  background.style.display = 'block';
-  exit.classList.add('exit');
-  body.style.overflowY = 'hidden';
+feedbacks.forEach(function (feedback) {
+  feedback.addEventListener('click', function () {
+    sideBar.classList.remove('sidebar-trans');
+    feedbackWindow.classList.add('open');
+    background.style.display = 'block';
+    exit.classList.add('exit');
+    body.style.overflowY = 'hidden';
+  });
 });
 
-exit.addEventListener('click', function () {
-   feedbackWindow.classList.remove('open');
-  background.style.display = 'none';
-  body.style.overflowY = 'visible';
+callbacks.forEach(function (callback) {
+  callback.addEventListener('click', function () {
+    sideBar.classList.remove('sidebar-trans');
+    callbackWindow.classList.add('open');
+    background.style.display = 'block';
+    exit.classList.add('exit');
+    body.style.overflowY = 'hidden';
+  });
 });
+
+  exit.addEventListener('click', function () {
+     feedbackWindow.classList.remove('open');
+     callbackWindow.classList.remove('open');
+    background.style.display = 'none';
+    body.style.overflowY = 'visible';
+  });
+
 
 background.addEventListener('click', function () {
    feedbackWindow.classList.remove('open');
+   callbackWindow.classList.remove('open');
   background.style.display = 'none';
   body.style.overflowY = 'visible';
 });
